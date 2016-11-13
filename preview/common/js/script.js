4c91c28de3372d741bea039ff4dd8439bdfdbee7
畑峯　豪 <gouhatamine@sunnysunnywrap.com>
2016-11-13 19:13:28 +0900

    全ファイル削除

--- preview/common/js/script.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$('.mainvisual-bg').stop().animate({'background-position':'+=' + '-1800' + 'px'},360000);
-	setInterval(function(){
-		$('.mainvisual-bg').stop().animate({'background-position':'+=' + '-1800' + 'px'},360000);
-	},360000);
-});
